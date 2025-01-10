ae7609354ec25871cac1a625dde7d88b50e1f9f4
IHyeon Hong <ihyeonhong0427@gmail.com>
2025-01-10 12:14:26 +0900

    Update : Add configuration that does not use Spring Data JPA when WebApplication.java

--- src/main/java/study/springboot/web/WebApplication.java
+++ src/main/java/study/springboot/web/WebApplication.java
@@ -2,12 +2,14 @@ package study.springboot.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+// Not using Spring Data JPA
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class WebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
 
 }
