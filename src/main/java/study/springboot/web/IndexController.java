ae923e5caf72e5582108d01a3466dd5e12f180ea
IHyeon Hong <ihyeonhong0427@gmail.com>
2025-01-13 11:33:51 +0900

    CREATE : Add Thymeleaf

---
+++ src/main/java/study/springboot/web/IndexController.java
@@ -0,0 +1,20 @@
+package study.springboot.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class IndexController {
+    @GetMapping(path = "/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping(path = "spring")
+    public String spring() {
+        return "spring";
+    }
+
+}
